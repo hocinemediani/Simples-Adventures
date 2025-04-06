790f29e880ef595870de3ba84bf2893a7419554c
hocinemediani <dolcejeannarciss@gmail.com>
2025-04-06 16:04:36 +0200

    small refactoring changes

--- com/projetlong/gh02/GameFrame.java
+++ com/projetlong/gh02/GameFrame.java
@@ -1,3 +1,5 @@
+package com.projetlong.gh02;
+
 import java.awt.Canvas;
 import java.awt.Color;
 import java.awt.Graphics;
@@ -6,41 +8,45 @@ import javax.swing.JFrame;
 
 public class GameFrame extends JFrame {
 
-    /* Canvas onto which the frame will draw */
-    private Canvas canvas = new Canvas();
+    /* Canvas onto which the frame will draw. */
+    private final Canvas canvas = new Canvas();
 
 
-    /** Creates the game frame */
+    /** Constructor for the game frame. It initializes
+     * a window with a canvas and creates a BufferStrategy
+     * to render images on the window.
+     */
     public GameFrame() {
-        /* Option to terminate the program upon closing the window */
+        /* Option to terminate the program upon closing the window. */
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setTitle("Projet long");
 
-        /* Sets base height and centers the window */
+        /* Sets base height and centers the window. */
         this.setBounds(0, 0, 1000, 800);
         this.setLocationRelativeTo(null);
 
-        /* Maximises the window */
+        /* Maximises the window. */
         this.setExtendedState(JFrame.MAXIMIZED_BOTH);
 
         this.add(canvas);
         this.setVisible(true);
-        canvas.createBufferStrategy(4);
-        BufferStrategy bufferStrategy = canvas.getBufferStrategy();
 
+        /* Setting up the buffering strategy. */
+        canvas.createBufferStrategy(4);
+    }
+
+    /** The main game loop, called at each tick.
+     * Most of the game logic will be inside it.
+    */
+    public void Update() {
+        BufferStrategy bufferStrategy;
         int theta = 0;
         int radius = 200;
-        int xRect = 0;
-        int yRect = 0;
-        int ticks = 0;
 
         while (true) {
-            if ((ticks % 2) == 0) {
-                theta++;
-                xRect = (int) Math.floor(radius * Math.cos(theta));
-                yRect = (int) Math.floor(radius * Math.sin(theta));
-            }
-
+            theta++;
+            int xRect = (int) Math.floor(radius * Math.cos(theta));
+            int yRect = (int) Math.floor(radius * Math.sin(theta));
             bufferStrategy = canvas.getBufferStrategy();
             Graphics graphics = bufferStrategy.getDrawGraphics();
             super.paint(graphics);
@@ -48,13 +54,13 @@ public class GameFrame extends JFrame {
             graphics.fill3DRect(xRect + 675, yRect + 375, 100, 60, true);
             graphics.dispose();
             bufferStrategy.show();
-            ticks++;
         }
     }
 
 
     public static void main(String... args) {
-        new GameFrame();
+        GameFrame frame = new GameFrame();
+        frame.Update();
     }
 
 }
