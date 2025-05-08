fce6cd2c357120af02f693795e56b2f5666d00ae
hocinemediani <dolcejeannarciss@gmail.com>
2025-05-08 22:54:30 +0200

    created the NPC class

---
+++ com/projetlong/gh02/NPC.java
@@ -0,0 +1,14 @@
+package com.projetlong.gh02;
+
+public class NPC implements GameObject {
+
+    @Override
+    public void render(RenderHandler renderHandler, int scale) {}
+
+    @Override
+    public void update(GameFrame game) {}
+
+    @Override
+    public void transform(int dx, int dy, int dTheta) {}
+
+}
